585cd69d190477122b5bb2cff479ff396ad39b27
Obed Patient Habiyaremye <lobedpatient@gmail.com>
2023-04-02 19:00:10 +0200

    Create and storing reviewrating

--- Myshopapp/views.py
+++ Myshopapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from Myshopapp.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from Myshopapp.models import Product, ReviewRating
 from category.models import Category
 from Carts.views import _cart_id
 from django.db.models import Q
-
+from .forms import ReviewForm
+from django.contrib import messages
 from Carts.models import CartItem
 from django.http import HttpResponse
 # Create your views here.
@@ -56,3 +57,28 @@ def search(request):
         'product_count': product_count
     }
     return render(request, 'store/purchase.html',context)
+
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method =='POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.info(request, 'Thank you! Your Review has been Updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data= ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.info(request, 'Thank you! Your Review has been Submitted.')
+                return redirect(url)
